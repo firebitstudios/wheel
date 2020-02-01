7d9aa5d9367ef85ebcab037180935a8e1796eb79
firebitstudios <59999424+firebitstudios@users.noreply.github.com>
2020-02-01 15:31:18 +0300

    First version
    App uploaded!

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1580476870,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/sprite-sheet0.png",
+		"images/spin_btn-sheet0.png",
+		"images/k-sheet0.png",
+		"images/1Первыйэкран-sheet0.png",
+		"images/к2-sheet0.png",
+		"images/pointer-sheet0.png",
+		"images/prize_screen-sheet0.png",
+		"images/get_prize_btn-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
