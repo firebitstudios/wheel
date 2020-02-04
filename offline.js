f8cacddc2eb2e3bce0afbdf8b42673bdd4db05e6
firebitstudios <59999424+firebitstudios@users.noreply.github.com>
2020-02-04 15:34:42 +0300

    Add files via upload

--- offline.js
+++ offline.js
@@ -1,9 +1,10 @@
 ﻿{
-	"version": 1580811201,
+	"version": 1580819516,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
-		"jquery-2.1.1.min.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
 		"images/sprite-sheet0.png",
 		"images/spin_btn-sheet0.png",
 		"images/k-sheet0.png",
@@ -11,6 +12,12 @@
 		"images/к2-sheet0.png",
 		"images/pointer-sheet0.png",
 		"images/prize_screen-sheet0.png",
-		"images/get_prize_btn-sheet0.png"
+		"images/get_prize_btn-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
 	]
 }
