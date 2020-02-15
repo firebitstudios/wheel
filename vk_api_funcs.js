72052ec3a1932204ab0d55eae3a109900860eb8d
firebitstudios <59999424+firebitstudios@users.noreply.github.com>
2020-02-15 14:24:47 +0300

    Update vk_api_funcs.js

--- vk_api_funcs.js
+++ vk_api_funcs.js
@@ -1,6 +1,6 @@
 var ID;
 function GET_ID(){
     VK.api('users.get', {'fields': 'photo_50,first_name,last_name', 'v':'5.73'}, function(data){
-        console.log("Your VK id is: "+data.response[0].id);
-        ID = data.response[0].id}); 
-    return ID;}
+        console.log("Your id is: "+data.response[0].id);
+        return data.response[0].id});
+}
