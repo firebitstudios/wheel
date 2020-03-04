75c41460fb7239e6410263d16c338b1616ec30dc
firebitstudios <59999424+firebitstudios@users.noreply.github.com>
2020-03-05 02:07:59 +0300

    Update vk_api_funcs.js

--- vk_api_funcs.js
+++ vk_api_funcs.js
@@ -1,3 +1,4 @@
+var check_result_id = "0";
 var send = function (params, func){
     var ajax = new XMLHttpRequest();
     ajax.open('GET', 'https://bsmxtestopencart.000webhostapp.com/vk-game/vk.php?'+params, true);
@@ -20,7 +21,7 @@ function pauseBrowser(millis) {
 var check_id = function (id){
     send('id='+id, function (text){
         if (text == "yes"){
-            c2_callFunction("show_prize_screen");
+            check_result_id = "1";
         }
     });
 };
@@ -45,6 +46,10 @@ var write_id = function (id){
 function GET_ID(){
     VK.api('users.get', {'fields': 'photo_50,first_name,last_name', 'v':'5.73'}, function(data){
         check_id(data.response[0].id);
+        console.log(check_result_id);
+        if (check_result_id == "1"){
+            c2_callFunction("show_prize_screen");
+        }
     });
 }
 function APPEND_ID(){
